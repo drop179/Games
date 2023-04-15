2a7b7a6fc0ec3b2c3b68e6997f9236c442e4b065
Mesme <75756450+drop179@users.noreply.github.com>
2023-04-15 21:10:54 +0100

    Bit Map Game

---
+++ bitMapImageConverion.py
@@ -0,0 +1,48 @@
+v= False
+while v == False:
+  time = input("Enter the time in the format hr:mn  ")
+  if ":" not in time:
+    v = False
+  elif time.split(":")[0] > 23 or time.split(":")[1]>59 or time.split(":")[0]<0 or time.split(":")[1]<0:
+    v = False
+  else:
+    v = True1
+
+  time = input("Incorrect format.\nEnter the time in the format hr:mn  ")
+time = [*time]
+time = " ".join(time)
+three = ["01110","10001","00010","10001","01110"]
+four = ["10001","10001","11111","00001","00001"]
+five = ["01111","10000","01111","00001","11110"]
+six = ["00110","01000","11110","10001","01110"]
+seven = """11111
+00010
+00100
+01000
+10000"""
+eight = """01110
+10001
+01110
+10001
+01110"""
+nine = """
+01110
+10001
+01010
+00100
+01000"""
+colon = """1
+1
+0
+1
+1"""
+space = """0
+0
+0
+0
+0"""
+
+nums = {"0":zero,"1":one,"2":two,"3":three,"4":four,"5":five,"6":six, "7":seven,"8":eight,"9":nine," ":space,":":colon}
+bitTime = []
+for i in time:
+  bitTime.append(nums[i])
